67af61f88ceda4ac2d01ea0acd47bd829a837e36
mittomao <mittomao9823@gmail.com>
2020-05-08 21:05:36 +0700

    Edit Phan Nghe Nhac Vs Giao Dien Moi

--- 2.js
+++ 2.js
@@ -1,327 +1,199 @@
-$(function()
-{
-    var playerTrack = $("#player-track");
-	var bgArtwork = $('#bg-artwork');
-	var bgArtworkUrl;
-	var albumName = $('#album-name');
-	var trackName = $('#track-name');
-	var list = $('#list-album');
-	var albumArt = $('#album-art'), 
-		sArea = $('#s-area'),
-		seekBar = $('#seek-bar'),
-		trackTime = $('#track-time'),
-		insTime = $('#ins-time'),
-		sHover = $('#s-hover'),
-		playPauseButton = $("#play-pause-button"),
-		i = playPauseButton.find('i'),
-		tProgress = $('#current-time'),
-		tTime = $('#track-length'),
-		seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0,
-		buffInterval = null, tFlag = false;
-	
-	var playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
-	
-	var songs = [{
-        stt : 0,
-		artist: "Track 1",
-		name: "Từ Khi Gặp Em - Trịnh Thăng Bình",
-		url: "Music/Tu_Khi_Gap_Em.mp3",
-		picture: "3.jpg"
-    },
-    {
-        stt : 1,
-		artist: "Track 2",
-		name: "Thương Em Là Điều Anh Không Ngờ Tới",
-		url: "Music/ThuongEmLaDieuAnhKhongTheNgo.mp3",
-		picture: "4.jpg"
-    },
-    {
-        stt : 2,
-		artist: "Track 3",
-		name: "OST Có Tôi Ở Đây Rùi ! ",
-		url: "Music/Co_Toi_O_Day_Roi.mp3",
-		picture: "6.jpg"
-    },
-    {
-        stt : 3,
-		artist: "Track 4",
-		name: "Yêu Từ Xa  -  Hồ Việ Trung",
-		url: "Music/Yeu_Tu_Xa.mp3",
-		picture: "5.jpg"
-    },
-    {
-        stt : 4,
-		artist: "Track 5",
-		name: "Ác Ma Đến Từ Thiên Đường",
-		url: "Music/Ac_Ma_Tu_Thien_Duong.mp3",
-		picture: "2.jpg"
-    }
-];
+const player = document.querySelector("#player");
+const progressBar = document.querySelector(".progressbar");
+const progress = document.querySelector(".progress-time");
+const progressVolum = document.querySelector(".progressVolum");
+const progressV = document.querySelector(".progressV");
+const curTime = document.querySelector("#curtime");
+const durTime = document.querySelector("#durtime");
+const playButton = document.querySelector("#playerButton");
+const loopButton = document.querySelector("#loop");
+const volumButton = document.querySelector("#volum");
+const titleMusic = document.querySelector(".title-music");
+const controls = document.querySelector(".controls");
+var duration = 0;
 
-	// Xáo Trộn Bài Hát
-	function shuffle(a) {
-		var j, x, i;
-		for (i = a.length - 1; i > 0; i--) {
-			j = Math.floor(Math.random() * (i + 1));
-			x = a[i];
-			a[i] = a[j];
-			a[j] = x;
-		}
-		return a;
-	}
-	// songs = shuffle(songs);
-    // Hàm Điểu Khiển Bài Hát
-    function playPause()
-    {
-        setTimeout(function()
-        {
-            if(audio.paused)
-            {
-                playerTrack.addClass('active');
-                albumArt.addClass('active');
-                checkBuffering();
-                i.attr('class','fa fa-pause');
-                audio.play();
-            }
-            else
-            {
-                playerTrack.removeClass('active');
-                albumArt.removeClass('active');
-                clearInterval(buffInterval);
-                albumArt.removeClass('buffering');
-                i.attr('class','fa fa-play');
-                audio.pause();
-            }
-        },300);
-    }
+// Lap Bai Hat 
 
-    	// Hiện Thị Thời Gian Của Bài Hát
-	function showHover(event)
-	{
-		seekBarPos = sArea.offset(); 
-		seekT = event.clientX - seekBarPos.left;
-		seekLoc = audio.duration * (seekT / sArea.outerWidth());
-		
-		sHover.width(seekT);
-		
-		cM = seekLoc / 60;
-		
-		ctMinutes = Math.floor(cM);
-		ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
-		
-		if( (ctMinutes < 0) || (ctSeconds < 0) )
-			return;
-		
-        if( (ctMinutes < 0) || (ctSeconds < 0) )
-			return;
-		
-		if(ctMinutes < 10)
-			ctMinutes = '0'+ctMinutes;
-		if(ctSeconds < 10)
-			ctSeconds = '0'+ctSeconds;
-        
-        if( isNaN(ctMinutes) || isNaN(ctSeconds) )
-            insTime.text('--:--');
-        else
-		    insTime.text(ctMinutes+':'+ctSeconds);
-            
-		// insTime.css({'left':seekT,'margin-left':'-21px'}).fadeIn(0);
-		
-	}
-
-    function hideHover()
-	{
-        sHover.width(0);
-        // insTime.text('00:00').css({'left':'0px','margin-left':'0px'}).fadeOut(0);		
-    }
-    // Tua Loi Bai Hat
-    function playFromClickedPos()
-    {
-        audio.currentTime = seekLoc;
-		seekBar.width(seekT);
-		hideHover();
-    }
-
-    function updateCurrTime()
-	{
-        nTime = new Date();
-        nTime = nTime.getTime();
-
-        if( !tFlag )
-        {
-            tFlag = true;
-            trackTime.addClass('active');
-        }
-
-		curMinutes = Math.floor(audio.currentTime / 60);
-		curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
-		
-		durMinutes = Math.floor(audio.duration / 60);
-		durSeconds = Math.floor(audio.duration - durMinutes * 60);
-		
-		playProgress = (audio.currentTime / audio.duration) * 100;
-		
-		if(curMinutes < 10)
-			curMinutes = '0'+curMinutes;
-		if(curSeconds < 10)
-			curSeconds = '0'+curSeconds;
-		
-		if(durMinutes < 10)
-			durMinutes = '0'+durMinutes;
-		if(durSeconds < 10)
-			durSeconds = '0'+durSeconds;
-        
-        if( isNaN(curMinutes) || isNaN(curSeconds) )
-            tProgress.text('00:00');
-        else
-		    tProgress.text(curMinutes+':'+curSeconds);
-        
-        if( isNaN(durMinutes) || isNaN(durSeconds) )
-            tTime.text('00:00');
-        else
-		    tTime.text(durMinutes+':'+durSeconds);
-        
-        if( isNaN(curMinutes) || isNaN(curSeconds) || isNaN(durMinutes) || isNaN(durSeconds) )
-            trackTime.removeClass('active');
-        else
-            trackTime.addClass('active');
-
-        
-		seekBar.width(playProgress+'%');
-		
-		if( playProgress == 100 )
-		{
-			i.attr('class','fa fa-play');
-			seekBar.width(0);
-            tProgress.text('00:00');
-            albumArt.removeClass('buffering').removeClass('active');
-            clearInterval(buffInterval);
-			selectTrack(1);
-		}
-    }
+loopButton.onclick = function(){
+    player.currentTime =0;
     
-    function checkBuffering()
-    {
-        clearInterval(buffInterval);
-        buffInterval = setInterval(function()
-        { 
-            if( (nTime == 0) || (bTime - nTime) > 1000  )
-                albumArt.addClass('buffering');
-            else
-                albumArt.removeClass('buffering');
-
-            bTime = new Date();
-            bTime = bTime.getTime();
-
-        },100);
+}
+playButton.onclick = function(){
+    if(this.classList.contains("fa-play-circle")){
+        this.classList.replace("fa-play-circle","fa-pause-circle");
+        player.play();
     }
-
-    function selectTrack(flag)
-    {
-        if( flag == 0 || flag == 1 )
-            ++currIndex;
-        else
-            --currIndex;
-
-        if( (currIndex > -1) && (currIndex < songs.length) )
-        {
-            if( flag == 0 )
-                i.attr('class','fa fa-play');
-            else
-            {
-                albumArt.removeClass('buffering');
-                i.attr('class','fa fa-pause');
-            }
-
-            seekBar.width(0);
-            trackTime.removeClass('active');
-            tProgress.text('00:00');
-            tTime.text('00:00');
-			
-            currAlbum = songs[currIndex].name; 
-            currTrackName = songs[currIndex].artist;
-            currArtwork = songs[currIndex].picture;
-            audio.src = songs[currIndex].url;
-            
-            nTime = 0;
-            bTime = new Date();
-            bTime = bTime.getTime();
-            document.getElementById(`stt_${currIndex}`).classList.toggle("hehe");
-
-            if(flag != 0)
-            {
-                audio.play();
-                playerTrack.addClass('active');
-                albumArt.addClass('active');
-            
-                clearInterval(buffInterval);
-                checkBuffering();
-            }
-            albumName.html(`<marquee behavior="smoth" direction="left">${currAlbum}</marquee>`);
-            trackName.text(currTrackName);
-            $('#album-art img').prop('src', `./Music/Picture/${currArtwork}`);
-            bgArtwork.prop('style',`background-image: url("./Music/Picture/${currArtwork}")`);
-        }
-        else
-        {
-            if( flag == 0 || flag == 1 )
-                --currIndex;
-            else
-                ++currIndex;
-        }
+    else{
+        this.classList.replace("fa-pause-circle","fa-play-circle");
+        player.pause();
     }
-    // Ham Show List AlbumArt
-    function showList(){
-        let i = -1;
-        let str = songs.reduce((acc,item)=>{
-            i++;
-            return acc + `<li class= "abc"><i class="fa fa-play" aria-hidden="true"></i><h5 class = "${i===0?'fisrt':'h5'}" id = "stt_${i}">${item.name}</h5></li>`;
-        },"");
-    
-        list.html(str);
-          
+}
+volumButton.onclick = function(){
+    progressVolum.classList.toggle("active");
+    if(this.classList.contains("fa-volume-up")){
+        this.classList.replace("fa-volume-up","fa-volume-off");
+        // player.volume = 0.3;
     }
-// Hàm Main
-    $(window).on( "load", showList)
-    function initPlayer()
-	{	
-        audio = new Audio();
+    else{
+        this.classList.replace("fa-volume-off","fa-volume-up");
+        // player.volume = 1;
 
-        selectTrack(0);
-        
-        showList();
-
-		audio.loop = true;
-		
-		playPauseButton.on('click',playPause);
-		
-		sArea.mousemove(function(event){ showHover(event); });
-		
-        sArea.mouseout(hideHover);
-
-        
-        sArea.on('click',playFromClickedPos);
-		
-        $(audio).on('timeupdate',updateCurrTime);
-  
-
-        playPreviousTrackButton.on('click',function(){ 
-            h5 = $(".h5");
-            h5.removeClass("hehe");
-            selectTrack(-1);
-            
-        } );
-        playNextTrackButton.on('click',function(){ 
-            h5 = $(".h5");
-            h5.removeClass("hehe");
-            h5.removeClass("first");
-            selectTrack(1);
-            
-        });
-        
     }
-  
-    initPlayer();
+}
+
+player.onloadedmetadata = function(){
+    duration = player.duration; //so giay cua bai hat
+    // chuyen ve phut va giay
+    const min = Math.floor(duration/60);
+    const sec = Math.floor(duration%60);
+    durTime.innerHTML = `${min<10?'0'+min:min}:${sec<10?'0'+sec:sec}`;
+}
+
+player.addEventListener("timeupdate",function(){
+    let currentTime = this.currentTime;
+    const min = Math.floor(currentTime/60);
+    const sec = Math.floor(currentTime%60);
+    curTime.innerHTML = `${min<10?'0'+min:min}:${sec<10?'0'+sec:sec}`;
+    let widthTime = (currentTime/duration)*100;
+    progress.style.width = widthTime + "%";
 });
 
+let down = false;
+progressBar.addEventListener("click",function(e){
+    progress.style.width = e.offsetX + "px";
+    let percent = (e.offsetX/progressBar.offsetWidth)*100;
+    player.currentTime = percent*duration/100;
+});
 
+progressBar.addEventListener("mousedown",function(){
+    down =true;
+});
+progressBar.addEventListener("mouseup",function(){
+    down =true;
+
+});
+
+progressBar.addEventListener("mouseout",function(){
+    down =false;
+
+});
+progressBar.addEventListener("mousemove",function(e){
+    if(down)
+    {
+        progress.style.width = e.offsetX + "px";
+        let percent = (e.offsetX/progressBar.offsetWidth)*100;
+        player.currentTime = percent*duration/100;
+    }
+});
+
+// Cho Volume
+
+progressVolum.addEventListener("click",function(e){
+    progressV.style.width = e.offsetX + "px";
+    let percent = (e.offsetX/progressVolum.offsetWidth)*100;
+    player.volume = percent/100;
+});
+
+// List Song
+var songs = [{
+    stt : 0,
+    artist: "Track 1",
+    name: "Từ Khi Gặp Em - Trịnh Thăng Bình",
+    url: "Music/Tu_Khi_Gap_Em.mp3",
+    picture: "3.jpg"
+},
+{
+    stt : 1,
+    artist: "Track 2",
+    name: "Thương Em Là Điều Anh Không Ngờ Tới",
+    url: "Music/ThuongEmLaDieuAnhKhongTheNgo.mp3",
+    picture: "4.jpg"
+},
+{
+    stt : 2,
+    artist: "Track 3",
+    name: "Tất Cả Sẽ Thay Anh ! ",
+    url: "Music/Tat_Ca_Se_Thay_Em.mp3",
+    picture: "6.jpg"
+},
+{
+    stt : 3,
+    artist: "Track 4",
+    name: "Yêu Từ Xa  -  Hồ Việt Trung",
+    url: "Music/Yeu_Tu_Xa.mp3",
+    picture: "5.jpg"
+},
+{
+    stt : 4,
+    artist: "Track 5",
+    name: "Ác Ma Đến Từ Thiên Đường",
+    url: "Music/Ac_Ma_Tu_Thien_Duong.mp3",
+    picture: "2.jpg"
+}
+];
+
+var currIndex = -1;
+
+function showAlbum(data){
+    const listAlbum = document.querySelector("#list-album");
+    let str = "";
+    data.map((item,index)=>{
+        str+=`<li class="music" data-img = '${item.picture}' data-id ="${index}" data-title = "${item.name}" data-url = "${item.url}"><span class="fa fa-play"></span><p>Track ${index+1}: ${item.name}</p></li>`;
+        return str;
+    });
+    listAlbum.innerHTML = str;
+}
+// Show Ra Danh Sach
+showAlbum(songs);
+
+const musics = document.querySelectorAll(".music");
+musics.forEach(music=>{
+    music.onclick = function(){
+        testActive(music);
+        // controls.style.backgroundImage = `url(./Music/Picture/${music.getAttribute("data-img")})`; 
+        playButton.classList.replace("fa-play-circle","fa-pause-circle");
+        let url = music.getAttribute("data-url");
+        player.setAttribute("src" , url);
+        player.play();
+        titleMusic.innerHTML = `<marquee>${music.getAttribute("data-title")}</marquee>`;
+        currIndex = parseInt(music.getAttribute("data-id"));
+    }
+});
+
+function testActive(element){
+       
+        for (const item of musics) {
+            item.classList.remove("active_s");
+            item.childNodes[1].classList.remove("hehe");
+        }
+        
+        element.classList.add("active_s");
+        element.childNodes[1].classList.add("hehe");   
+}
+function stackMusic(url,title=""){
+    player.setAttribute("src",url);
+    titleMusic.innerHTML = title;
+}
+const prevButton = document.querySelector("#prevButton");
+prevButton.onclick = function(){
+    currIndex  = currIndex -1;
+    if(currIndex < 0)
+    {
+        currIndex=songs.length-1;
+    }
+    // testActive(currIndex,musics[currIndex]);
+    testActive(musics[currIndex]);
+    stackMusic(songs[currIndex].url , songs[currIndex].name);
+    player.play();
+}
+const nextButton = document.querySelector("#nextButton");
+nextButton.onclick = function(){
+    currIndex = currIndex + 1;
+    if(currIndex > songs.length-1)
+    {
+        currIndex=0;
+    }
+    testActive(musics[currIndex]);
+    stackMusic(songs[currIndex].url,songs[currIndex].name);
+    player.play();
+}
